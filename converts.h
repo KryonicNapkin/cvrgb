a08c086dc51f3d4ab2ff7cc5ef2b648584cd4b54
coevoe <grastlinka@gmail.com>
2024-08-10 08:21:41 +0200

    Removed unused structr convert_to_t

--- converts.h
+++ converts.h
@@ -6,13 +6,6 @@ typedef uint8_t col_val;
 typedef col_val* rgb_val;
 
 typedef enum {
-    TO_DEC,
-    TO_HEX,
-    TO_OCT,
-    TO_BIN,
-} convert_to_t;
-
-typedef enum {
     ERROR = -1,
     DECIMAL,
     HEXADECIMAL,
@@ -23,6 +16,5 @@ typedef enum {
 col_val any_to_dec(char* str, c_rgb_t type);
 rgb_val dconv_str_with_type(char** vals, c_rgb_t type);
 rgb_val get_rgb_val(char* str);
-char* rgb_val_convert_to(col_val rgb_val, convert_to_t type);
 
 #endif
